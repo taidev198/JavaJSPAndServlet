13f813867b3248ef4d05c3c62faa1a6f21f58300
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-05-27 21:20:56 +0700

    DAO Pattern

---
+++ src/main/java/com/example/taidev198/javaeehome/impl/NewsService.java
@@ -0,0 +1,6 @@
+package com.example.taidev198.javaeehome.impl;
+
+import com.example.taidev198.javaeehome.service.INewsService;
+
+public class NewsService implements INewsService {
+}
---
+++ src/main/java/com/example/taidev198/javaeehome/service/INewsService.java
@@ -0,0 +1,4 @@
+package com.example.taidev198.javaeehome.service;
+
+public interface INewsService {
+}
