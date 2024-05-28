b03298c1c0d503d167387e786e060509273bd947
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-05-28 14:18:29 +0700

    display data to jsp

--- src/main/java/com/example/taidev198/javaeehome/dao/AbstractDAO.java
+++ src/main/java/com/example/taidev198/javaeehome/dao/AbstractDAO.java
@@ -2,6 +2,12 @@ package com.example.taidev198.javaeehome.dao;
 
 public class AbstractDAO<T> {
 
+        void update(T entity) {
 
+        }
+
+        void save(T entity) {
+
+        }
 
 }
--- src/main/java/com/example/taidev198/javaeehome/impl/NewsDAO.java
+++ src/main/java/com/example/taidev198/javaeehome/impl/NewsDAO.java
@@ -8,59 +8,67 @@ import com.example.taidev198.javaeehome.model.NewsModel;
 import java.util.List;
 
 public class NewsDAO extends AbstractDAO<NewsModel> implements INewsDAO {
+
+
     @Override
     public NewsModel findOneById(Long id) {
         String sql = "select * from news where id = ?";
-        List<NewsModel> news = query(sql, new NewsMapper(), id);
-        return news.isEmpty() ? null : news.get(0);
+//        List<NewsModel> news = query(sql, new NewsMapper(), id);
+//        return news.isEmpty() ? null : news.get(0);
+
+        return null;
     }
 
     @Override
     public List<NewsModel> findByCategoryId(Long categoryId) {
         String sql = "select * from news where category_id=?";
-        return query(sql, new NewsMapper(), categoryId);
+       // return query(sql, new NewsMapper(), categoryId);
+        return null;
     }
 
     @Override
     public List<NewsModel> findAll() {
         String sql = "select * from news ";
-        return query(sql, new NewsMapper());
+       // return query(sql, new NewsMapper());
+        return null;
     }
 
     @Override
     public Long save(NewsModel newsModel) {
         StringBuilder sql = new StringBuilder("insert into news(title,content, thumbnails,shortdescription,category_id,createddate,modifieddate,createdby,modifiedby) ");
         sql.append(" values(?,?,?, ?, ?, ?, ?, ?, ?)");
-        return insert(sql.toString(),
-                newsModel.getTitle(),
-                newsModel.getContent(),
-                newsModel.getThumbnails(),
-                newsModel.getShortDescription(),
-                newsModel.getCategoryId(),
-                newsModel.getCreatedDate(),
-                newsModel.getModifiedDate(),
-                newsModel.getCreatedBy(),
-                newsModel.getModifiedBy());
+//        return insert(sql.toString(),
+//                newsModel.getTitle(),
+//                newsModel.getContent(),
+//                newsModel.getThumbnails(),
+//                newsModel.getShortDescription(),
+//                newsModel.getCategoryId(),
+//                newsModel.getCreatedDate(),
+//                newsModel.getModifiedDate(),
+//                newsModel.getCreatedBy(),
+//                newsModel.getModifiedBy());
+
+        return null;
     }
 
     @Override
     public void delete(Long id) {
         String sql = "delete from news where id=?";
-        update(sql, id);
+       // update(sql, id);
     }
 
     @Override
     public void update(NewsModel newsModel) {
         StringBuilder sql = new StringBuilder("update news set title =?,content =?, thumbnails = ?,shortdescription= ?,category_id =?,createddate=?,modifieddate=?,createdby=?,modifiedby=? ");
-        return update(sql.toString(),
-                newsModel.getTitle(),
-                newsModel.getContent(),
-                newsModel.getThumbnails(),
-                newsModel.getShortDescription(),
-                newsModel.getCategoryId(),
-                newsModel.getCreatedDate(),
-                newsModel.getModifiedDate(),
-                newsModel.getCreatedBy(),
-                newsModel.getModifiedBy());
+//         update(sql.toString(),
+//                newsModel.getTitle(),
+//                newsModel.getContent(),
+//                newsModel.getThumbnails(),
+//                newsModel.getShortDescription(),
+//                newsModel.getCategoryId(),
+//                newsModel.getCreatedDate(),
+//                newsModel.getModifiedDate(),
+//                newsModel.getCreatedBy(),
+//                newsModel.getModifiedBy());
     }
 }
