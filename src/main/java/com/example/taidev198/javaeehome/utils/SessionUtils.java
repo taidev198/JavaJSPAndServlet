acb3aaa04770f3f8ad966061bbe53d3c5bb39ec4
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-05-29 16:17:20 +0700

    creating session utils

--- src/main/java/com/example/taidev198/javaeehome/utils/SessionUtils.java
+++ src/main/java/com/example/taidev198/javaeehome/utils/SessionUtils.java
@@ -1,18 +1,33 @@
 package com.example.taidev198.javaeehome.utils;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 //to resolve the generic problem
 public class SessionUtils {
 
-    public void putValue(String key,Object value){
+    private static SessionUtils sessionUtils = null;
 
+    public static SessionUtils getInstance() {
+        if (sessionUtils == null) {
+            synchronized (SessionUtils.class) {
+                if (sessionUtils == null) {
+                    sessionUtils = new SessionUtils();
+                }
+            }
+        }
+        return sessionUtils;
     }
 
-    public Object getValue(String key){
-        return null;
+    public void putValue(HttpServletRequest request, String key, Object value){
+        request.getSession().setAttribute(key,value);
     }
 
-    public void removeValue(String key){
+    public Object getValue(HttpServletRequest request, String key){
+        return request.getSession().getAttribute(key);
+    }
 
+    public void removeValue(HttpServletRequest request, String key){
+        request.getSession().removeAttribute(key);
     }
 
 }
